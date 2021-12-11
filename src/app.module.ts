e11f11cc9a10eeec6f5bddeda3ee5f97135f74ce
netoudi <netoudi@rocketmail.com>
2021-12-11 16:32:44 -0300

    Add module Accounts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
+import { AccountsModule } from './accounts/accounts.module';
+import { Account } from './accounts/entities/account.entity';
 
 @Module({
   imports: [
     OrdersModule,
+    AccountsModule,
     SequelizeModule.forRoot({
       dialect: 'sqlite',
       host: join(__dirname, 'database.sqlite'),
       autoLoadModels: true,
-      models: [Order],
+      models: [Order, Account],
+      sync: {
+        alter: true,
+        force: true, // don't use in production
+      },
     }),
   ],
   controllers: [AppController],
